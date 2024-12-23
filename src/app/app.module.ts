9c7158b7d6fd1a5a2e43a4f7bc3514d35730df73
OrlandoBurgos34 <programador3423@gmail.com>
2024-12-23 10:21:45 -0500

    generate components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LayoutComponent } from './layout/layout.component';
+import { NabvarComponent } from './layout/nabvar/nabvar.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HomeComponent } from './component/home/home.component';
+import { ContactComponent } from './component/contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    NabvarComponent,
+    FooterComponent,
+    HomeComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
